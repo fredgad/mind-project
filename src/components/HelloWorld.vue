a2e877b45326c6c6f7c130d641c99eaac3ddbaf4
Fred <fred_gad@mail.ru>
2019-06-11 12:01:54 +0300

    Init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+.hello {
+  border: 2px solid red;
+
+      h1 {
+        border: 20px solid green;
+      }
+}
+</style>
